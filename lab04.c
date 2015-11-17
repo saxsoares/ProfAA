207e1917aebfeca2b618f5ebc450507ac650e610
sax.soares <sax.soares@gmail.com>
2015-11-16 22:22:28 -0200

    aaatualizado

--- lab04.c
+++ lab04.c
@@ -1,84 +1,111 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int compara(x, y)
-
-void *x, *y; /* Declaração antiga do ANSI C, mas muito útil */
-{
-    if ( *(int*)x > *(int*)y )
-       return 1;
-    else if ( *(int*)x == *(int*)y )
-            return 0;
-    else if ( *(int*)x < *(int*)y )
-            return -1;
-}
 //
-
 /* representação de uma aresta */
-typedef struct edge* apEdge;
-typedef struct edge {
-int c; /* 'peso' ou 'custo' associado à aresta */
-9
-int v; /* índice do vértice 'destino' */
-apEdge next; /* próxima aresta da lista */
-} edge;
+typedef struct SEdge *apEdge;
+typedef struct SEdge
+{
+    int c; /* 'peso' ou 'custo' associado à aresta */
+    int indicado; /* índice do vértice 'destino' */
+    apEdge next; /* próxima aresta da lista */
+} TEdge;
 
 /* descrição de um vértice */
-typedef struct vert {
-int d; /* 'distância' do vértice à 'origem' */
-10
-int r; /* próximo vértice no caminho até a origem */
-int m; /* vértice marcado ? */
-apEdge list; /* lista de arestas */
-} vert;
-
-
-int remove(int x, int *seraconv, int *auxconvidado)
+//typedef struct SVert *Grafo;
+typedef struct SVert
 {
-    seraconv[x] = 0;
-    return auxconvidado[x];
+    int id;
+
+
+    int d; /* 'distância' do vértice à 'origem' */
+    int r; /* próximo vértice no caminho até a origem */
+    int m; /* vértice marcado ? */
+    apEdge list; /* lista de arestas */
+} TVert;
+
+typedef struct SGrafo
+{
+    TVert *Nos;
+    TEdge *Arestas;
+    int flagsInd;
+}TGrafo;
+
+apEdge InicializaVe()
+{
+    apEdge aresta;
+    aresta = (apEdge)malloc(sizeof(TEdge));
+    aresta->next = NULL;
+    return aresta;
 }
 
-MaximalInducedSubgraph(G, k)
+TVert InicializaNo()
 {
-    if (n<k+1)
-        H = ∅
-    else
-    if(every vertex of G has degree >= k)
-        H = G
-    else
-    {
-        v = vertex wih degree < k
-        H = MaximalInducedSubgraph(G-v, k)
-    }
-    return (H)
+	TVert No;
+
+	No = (TVert) malloc(sizeof(TVert));
+	No->m = 0;
+	No->list = InicializaVe();
+
+	return No;
 }
+
+int Carrega(TVert *G)
+{
+    int npessoas;
+
+    scanf("%d", &npessoas);
+
+    return npessoas;
+}
+
+//MaximalInducedSubgraph(Grafo G, k)
+//{
+//    if (n<k+1)      // !find 1
+//        H = ∅
+//    else
+//    if(every vertex of G has degree >= k) // !find 0
+//        H = G
+//    else
+//    {
+//        v = vertex wih degree < k
+//        H = MaximalInducedSubgraph(G-v, k)
+//    }
+//    return (H)
+//}
 
 int main()
 {
-    int nconvidados, indica, i;
-    int *convidado, *auxconvidado, *seraconv; // convidado armazena quem o item indicou, indicado armazena por quem foi indicado
+    Grafo G;
+    TGrafo *grafo;
+    int npessoas, nindicados;
 
-    scanf("%d", &nconvidados);
-    nconvidados = nconvidados + 1;
+    G = InicializaNo();
 
-    convidado = (int *) malloc(nconvidados* sizeof(int));
-    auxconvidado = (int *) malloc(nconvidados* sizeof(int));
-    seraconv = (int *) malloc(nconvidados* sizeof(int));
+    npessoas = Carrega(&G);
+    grafo = (TGrafo)malloc(sizeof(SGrafo));
+    grafo.Nos = (TVert)malloc(n*sizeof(SVert));
+//    int nconvidados, indica, i;
+//    int *convidado, *auxconvidado, *seraconv; // convidado armazena quem o item indicou, indicado armazena por quem foi indicado
 
-    for(i=0; i<nconvidados; i++)
-        seraconv[i] = 1;
 
-    for(i=0; i<nconvidados; i++)
-    {
-        scanf("%d", &indica); // atual que esta indicando
-        scanf("%d", &convidado[indica]); // quem ele indicou
-        auxconvidado[indica] = convidado[indica];
-
-    }
-
-    qsort(convidado, nconvidados, sizeof(int), compara);
-
-}
-
+//    scanf("%d", &nconvidados);
+//    nconvidados = nconvidados + 1;
+//
+//    convidado = (int *) malloc(nconvidados* sizeof(int));
+//    auxconvidado = (int *) malloc(nconvidados* sizeof(int));
+//    seraconv = (int *) malloc(nconvidados* sizeof(int));
+//
+//    for(i=0; i<nconvidados; i++)
+//        seraconv[i] = 1;
+//
+//    for(i=0; i<nconvidados; i++)
+//    {
+//        scanf("%d", &indica); // atual que esta indicando
+//        scanf("%d", &convidado[indica]); // quem ele indicou
+//        auxconvidado[indica] = convidado[indica];
+//
+//    }
+//
+//    qsort(convidado, nconvidados, sizeof(int), compara);
 }
---
+++ lab04_02.c
@@ -0,0 +1,117 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+
+typedef struct SAresta *Aresta;
+typedef struct SAresta
+{
+    int id;
+    Aresta next;
+}TAresta;
+
+typedef struct SPessoa *Pessoa;
+typedef struct SPessoa
+{
+    int pindicada;
+    Aresta Indicadapor;
+}TPessoa;
+
+typedef struct SGrafo *Grafo;
+typedef struct SGrafo
+{
+    Pessoa Pessoa;
+    Aresta Aresta;
+}TGrafo;
+
+Grafo AlocaG(int n)
+{
+    return n > 0 ? (Grafo)malloc(n*sizeof(TGrafo)) : NULL ;
+}
+
+Pessoa AlocaP(int n)
+{
+    return (n > 0) ? (Pessoa)malloc(n*sizeof(TPessoa)) : NULL ;
+}
+
+Aresta AlocaA(int n)
+{
+    return (n > 0) ? (Aresta)malloc(n*sizeof(TAresta)) : NULL ;
+}
+
+Aresta InsereAresta(int id, Aresta X)
+{
+    if(!X)
+    {
+        X = AlocaA(1);
+        X->id = id;
+        X->next = NULL;
+    }
+    else
+    {
+        X->next = InsereAresta(id, X->next);
+    }
+    return X;
+}
+
+void ImprimeAresta(Aresta X)
+{
+    if(X)
+    {
+        printf("indicada por %d\n", X->id);
+        if(X->next)
+            ImprimeAresta(X->next);
+    }
+}
+//MaximalInducedSubgraph(Grafo G, k)
+//{
+//    if (n<k+1)      // !find 1
+//        H = ∅
+//    else
+//    if(every vertex of G has degree >= k) // !find 0
+//        H = G
+//    else
+//    {
+//        v = vertex wih degree < k
+//        H = MaximalInducedSubgraph(G-v, k)
+//    }
+//    return (H)
+//}
+
+
+int main()
+{
+    int npessoas, i, j, k;
+    int pessoaid, indica;
+    Grafo G;
+    G = AlocaG(1);
+
+    scanf("%d", &npessoas);         // primeira entrada, numero de pessoas
+    npessoas++;
+    G->Pessoa = AlocaP(npessoas);
+    G->Aresta = AlocaA(npessoas);
+
+    // Inicializando Pessoas
+    for(i = 0; i < (npessoas); i++)
+    {
+        G->Pessoa[i].Indicadapor = NULL;
+        G->Aresta[i].next = NULL;
+        G->Aresta[i].id = 0;
+    }
+
+    for(i = 1; i < (npessoas); i++)
+    {
+        scanf("%d", &pessoaid);     // indice da pessoa atual que fará sua indicação
+        scanf("%d", &indica);       // pessoa que a atual indicou
+        G->Pessoa[pessoaid].pindicada = indica;
+        // Inserindo Aresta ->indicada por, na pessoa indicada
+        G->Pessoa[indica].Indicadapor = InsereAresta(pessoaid, G->Pessoa[indica].Indicadapor); // armazenando na pessoa indicada a pessoa que o indicou
+    }
+
+      for(i = 1; i < npessoas; i++)
+    {
+        printf("%d %d\n", i, G->Pessoa[i].pindicada);
+        printf("pessoa %d  - \n",i);
+        ImprimeAresta(G->Pessoa[i].Indicadapor);
+        printf("\n");
+    }
+}
